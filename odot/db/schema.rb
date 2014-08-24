3b48cd1e14fd6fc0b9ca46370b4ccb4d674e006b
kevinegstorf <kevinegstorf@kevins-MacBook-Pro.local>
2014-08-24 09:44:59 +0200

    added controller but is not working

--- odot/db/schema.rb
+++ odot/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140823135749) do
+ActiveRecord::Schema.define(version: 20131209232612) do
 
   create_table "todo_items", force: true do |t|
     t.integer  "todo_list_id"
