ccab98e2c5f723a4851ab1c1049b8ec3f36cc550
Kevin Egstorf git config --global user.email you@example.cogit config --global user.name Kevin <kevin.egstorf@gmail.com>
2014-09-20 10:26:32 +0200

    added user support to app

--- odot/app/controllers/user_sessions_controller.rb
+++ odot/app/controllers/user_sessions_controller.rb
@@ -2,7 +2,15 @@ class UserSessionsController < ApplicationController
   def new
   end
 
-  def crate
-  	redirect_to todo_lists_path
+  def create
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      flash[:success] = "Thanks for logging in!"
+      redirect_to todo_lists_path
+    else
+      flash[:error] = "There was a problem logging in. Please check your email and password."
+      render action: 'new'
+    end
   end
 end
